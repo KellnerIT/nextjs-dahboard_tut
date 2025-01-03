89af7ca48b87e432fd69220e49d4b9c1760e3435
Noah Kellner <noah@kellnerit.de>
2025-01-03 19:54:00 +0100

    Update next-auth to version 5.0.0-beta.25

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,6 +5,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 //import { useActionState } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
